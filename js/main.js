a082a363f21d7c43eae09662444c0c3216d613a0
Loic Pennequin <lo.pennequin@gmail.com>
2017-04-26 20:44:45 +0200

    fixed and loading screen test

--- js/main.js
+++ js/main.js
@@ -1,51 +1,17 @@
 $(document).ready(function(){
 
-  var introOffset = $('#intro').offset().top;
-  var chap1Offset = $('#home_ch1').offset().top;
-  var chap2Offset = $('#home_ch2').offset().top;
-  var chap3Offset = $('#home_ch3').offset().top;
-  var chap4Offset = $('#home_ch4').offset().top;
 
-  console.log(chap3Offset, chap4Offset);
 
-//APPARITION LEXIQUE ET SURLIGNAGE NAVBAR
+//PRELOADING
+$('.main-wrapper').imagesLoaded( function(){
+  $('.preload').addClass('loader-hidden');
+  $('.main-wrapper').show();
     $(window).scroll(function() {
-
       if ($(window).scrollTop() > $('#intro').height()-100){
          $(".lexique-wrapper").css('opacity', '1')
        }
-
-       if ( $(window).scrollTop() > introOffset && $(window).scrollTop() < chap1Offset){
-         $("#intro-btn").css('color', 'red');
-       }else{
-         $("#intro-btn").css('color', 'black');
-       }
-
-       if ( $(window).scrollTop() >chap1Offset && $(window).scrollTop() < chap2Offset){
-         $("#ch1-btn").css('color', 'red');
-       }else{
-         $("#ch1-btn").css('color', 'black');
-       }
-
-       if ( $(window).scrollTop() >chap2Offset && $(window).scrollTop() < chap3Offset){
-         $("#ch2-btn").css('color', 'red');
-       }else{
-         $("#ch2-btn").css('color', 'black');
-       }
-
-       if ( $(window).scrollTop() >chap3Offset && $(window).scrollTop() < chap4Offset){
-         $("#ch3-btn").css('color', 'red');
-       }else{
-         $("#ch3-btn").css('color', 'black');
-       }
-
-       if ( $(window).scrollTop() >chap4Offset){
-         $("#ch4-btn").css('color', 'red');
-       }else{
-         $("#ch4-btn").css('color', 'black');
-       }
-
      });
+})
 
 
 //GESTION LEXIQUE
@@ -99,10 +65,10 @@ $(document).ready(function(){
     })
 
 
- // LAZYLOAD
- $("img.lazy").lazyload({
-     effect : "fadeIn",
-     threshold : 200
- });
+ // // LAZYLOAD
+ // $("img.lazy").lazyload({
+ //     effect : "fadeIn",
+ //     threshold : 200
+ // });
 
 });
